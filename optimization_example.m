8928fc36b89d38ed912ca223e250f00a8f069aac
Bowen Zhang <61903437+Louis317@users.noreply.github.com>
2024-11-13 18:19:06 +0100

    Add files via upload

---
+++ optimization_example.m
@@ -0,0 +1,23 @@
+% Optimization Example
+
+r = 0.5:0.1:10; 			%radius
+V = 330;				    %volume
+
+A = 2*pi*r.^2 + 2*V./r;		%surface area
+  
+[opt_r,i] = min(A);         %find optimal r
+
+opt_h = V/(pi*opt_r^2);
+
+display(['The optimal radius is: ',num2str(r(i)),'cm',', the optimal height is: ',num2str(opt_h),'cm']);
+
+figure('Name','Optimization Example');
+plot(r,A);
+title('Surface area vs. radius')
+hold on
+plot(r(i),A(i),'r*')
+legend('A(r)','value of r that minimizes A')
+xlabel('r in cm')
+ylabel('A(r) in cm^2')
+grid on
+hold off
